6167db8840bb42ffd3b42fff00fec876c3015c26
rahul389 <rahul.sharma@surmountsoft.in>
2019-04-26 08:29:06 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -61,7 +61,12 @@ class ArticleController extends Controller
         }
 
         $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) :
-            new Article(); $article->id = $request->input('article_id');$article->title = $request->input('title');$article->body = $request->input('body'); if ($article->save()) { return redirect()->route('article.show', $article->id);
+            new Article();
+        $article->id = $request->input('article_id');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+        if ($article->save()) {
+            return redirect()->route('article.show', $article->id);
         }
     }
 
